303c5eeeea44ea3a28bef2830516b35efe0aa2b7
Jimmy Charnley Kromann <jimmy@charnley.dk>
2025-11-15 16:00:28 +0100

    Good UGE examples for different formats

--- tests/resources/uge/generate_uge_examples.py
+++ tests/resources/uge/generate_uge_examples.py
@@ -2,6 +2,7 @@ import time
 from pathlib import Path
 
 from hpc_funcs.schedulers.uge.constants import TASK_ENVIRONMENT_VARIABLE
+from hpc_funcs.schedulers.uge.monitoring import wait_for_jobs
 from hpc_funcs.schedulers.uge.qdel import delete_job
 from hpc_funcs.schedulers.uge.qsub import submit_script
 from hpc_funcs.schedulers.uge.submission import generate_taskarray_script
@@ -9,37 +10,6 @@ from hpc_funcs.shell import execute
 
 
 def generate_taskarray_log(global_tmp_path: Path):
-
-    # Need network accessible folder
-    tmp_path = global_tmp_path
-
-    # Generate bash script
-    success_string = "finished work"
-    command = f'sleep 5 & echo "{success_string} {TASK_ENVIRONMENT_VARIABLE}"'
-    log_dir = tmp_path / "uge_testlogs"
-
-    script: str = generate_taskarray_script(
-        command,
-        cores=1,
-        cwd=tmp_path,
-        log_dir=log_dir,
-        name="TestJob",
-        task_concurrent=1,
-        task_stop=2,
-    )
-
-    job_id, _ = submit_script(script, scr=tmp_path)
-
-    time.sleep(3)
-
-    execute(f"qstat -nenv -j {job_id} > {tmp_path}/qstat_jobinfo_array.txt")
-    execute(f"qstat -nenv -j {job_id} -xml > {tmp_path}/qstat_jobinfo_array.xml")
-    execute(f"qstat -nenv -j {job_id} -json > {tmp_path}/qstat_jobinfo_array.json")
-
-    delete_job(job_id)
-
-
-def generate_errorjob_log(global_tmp_path: Path):
 
     # Need network accessible folder
     tmp_path = global_tmp_path
@@ -60,7 +30,45 @@ def generate_errorjob_log(global_tmp_path: Path):
 
     job_id, _ = submit_script(script, scr=tmp_path)
 
-    time.sleep(20)
+    assert job_id is not None
+
+    time.sleep(3)
+
+    execute(f"qstat -nenv -j {job_id} > {tmp_path}/qstat_jobinfo_array.txt")
+    execute(f"qstat -nenv -j {job_id} -xml > {tmp_path}/qstat_jobinfo_array.xml")
+    execute(f"qstat -nenv -j {job_id} -json > {tmp_path}/qstat_jobinfo_array.json")
+
+    for fjob_id in wait_for_jobs([job_id], sleep=5):
+        print(f"job {fjob_id} finished")
+
+    execute(f"qacct -j {job_id} > {tmp_path}/qacct_array.txt")
+
+    delete_job(job_id)
+
+
+def generate_errorjob_log(global_tmp_path: Path):
+
+    # Need network accessible folder
+    tmp_path = global_tmp_path
+
+    # Generate bash script
+    command = f'set -e; sleep 1; if test "{TASK_ENVIRONMENT_VARIABLE}" == 2; then command_does_not_exist; else pwd; fi'
+    log_dir = Path("/log/dir/does/not/exist")
+
+    script: str = generate_taskarray_script(
+        command,
+        cores=1,
+        cwd=tmp_path,
+        log_dir=log_dir,
+        name="TestJob",
+        task_concurrent=1,
+        task_stop=2,
+        generate_dirs=False,
+    )
+
+    job_id, _ = submit_script(script, scr=tmp_path)
+
+    time.sleep(10)
 
     execute(f"qstat -nenv -j {job_id} > {tmp_path}/qstat_jobinfo_error.txt")
     execute(f"qstat -nenv -j {job_id} -xml > {tmp_path}/qstat_jobinfo_error.xml")
